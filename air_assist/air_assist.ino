4dc52346fd0d63dd7cfc47a44fea0483ac06a671
David Hempy <dhempy@users.noreply.github.com>
2020-04-06 00:31:55 -0400

    ADD GNU GPL v3 stmt to code

--- air_assist/air_assist.ino
+++ air_assist/air_assist.ino
@@ -4,7 +4,20 @@
 // It activates solenoid valves to assist a patient inhaling and exhaling.
 // It allows the practitioner to control the rate of respiration.
 //
-// Copyright (C) 2020 David Hempy - See LICENSE file
+// Copyright (C) 2020 David Hempy
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
 #include <LiquidCrystal.h>
