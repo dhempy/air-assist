8e418837aa4d4f6bc5023dc89ea3727c96d81e8e
David Hempy <dhempy@users.noreply.github.com>
2020-04-06 00:15:51 -0400

    Tidy

--- air_assist/air_assist.ino
+++ air_assist/air_assist.ino
@@ -30,12 +30,9 @@ const byte ZZZ_PIN               = A3;
 const byte LCD_RS_PIN            = A4;
 const byte LCD_ENABLE_PIN        = A5;
 
-int cycle_time  = 3000;
+long cycle_time  = 3000;
 float ie_ratio    = 2.0;
-
-// long inhale_time       = 1000;
 long inhale_hold_time  = 1000;
-// long exhale_time       = 3000;
 long exhale_hold_time  = 2000;
 
 enum state_enum {START, INHALE, EXHALE};
@@ -47,8 +44,6 @@ char current_state = START;
 
 char msg[255];
 
-
-// initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(LCD_ENABLE_PIN, LCD_RS_PIN,
   LCD_D4_PIN, LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
 
@@ -59,20 +54,15 @@ void setup() {
   Serial.begin(9600);
   Serial.println("\n----------------- Air Assist Activating -----------------\n");
 
-  // pinMode(RX_PIN,         QQQ);
-  // pinMode(TX_PIN,         QQQ);
   pinMode(BUTTON_UP_PIN,     INPUT_PULLUP);
   pinMode(BUTTON_DOWN_PIN,   INPUT_PULLUP);
   pinMode(BUTTON_RATIO_PIN,  INPUT_PULLUP);
+  pinMode(BUTTON_RATE_PIN,   INPUT_PULLUP);
+
   pinMode(RELAY_4_PIN,       OUTPUT);
   pinMode(RELAY_ALARM_PIN,   OUTPUT);
   pinMode(RELAY_EXHALE_PIN,  OUTPUT);
   pinMode(RELAY_INHALE_PIN,  OUTPUT);
-  pinMode(BUTTON_RATE_PIN,   INPUT_PULLUP);
-  // pinMode(LCD_D4_PIN,     QQQ);
-  // pinMode(LCD_D5_PIN,     QQQ);
-  // pinMode(LCD_D6_PIN,     QQQ);
-  // pinMode(LCD_D7_PIN,     QQQ);
 
   compute_delays();
 }
@@ -134,8 +124,6 @@ char button_stats[128];
 
 
 void check_buttons() {
-  // Serial.println("check_buttons");
-
   char up_state    = !digitalRead(BUTTON_UP_PIN);
   char down_state  = !digitalRead(BUTTON_DOWN_PIN);
   char ratio_state = !digitalRead(BUTTON_RATIO_PIN);
@@ -143,31 +131,15 @@ void check_buttons() {
   char up_pressed   = 0;
   char down_pressed = 0;
 
-  // bool activity = 0;
-
-  // sprintf(msg, "%ld up_state:%d last_up_state:%d up_pressed:%d (PRE)",
-  //              millis(), up_state, last_up_state, up_pressed);
-  // Serial.println(msg);
-
-
   if (up_state != last_up_state) {
     up_pressed = up_state;
     last_up_state = up_state;
   }
 
-  // sprintf(msg, "%ld up_state:%d last_up_state:%d up_pressed:%d (POST)",
-  //              millis(), up_state, last_up_state, up_pressed);
-  // Serial.println(msg);
-
   if (down_state != last_down_state) {
     down_pressed = down_state;
     last_down_state = down_state;
   }
-
-  // if (!(up_pressed ||down_pressed ||rate_state ||ratio_state)) {
-  //   return;
-  // }
-
 
   if (
       (rate_state && ratio_state) ||
@@ -191,29 +163,23 @@ void check_buttons() {
 }
 
 void rate_increase() {
-  // Serial.println("rate_increase");
   cycle_time = min(6000, cycle_time + 100);
 }
 
 void rate_decrease() {
-  // Serial.println("rate_decrease");
   cycle_time = max(2000, cycle_time - 100);
 }
 
 void ratio_increase() {
-  // Serial.println("ratio_increase");
   ie_ratio = min(3.0, ie_ratio + 0.1);
 }
 
 void ratio_decrease() {
-  // Serial.println("ratio_decrease");
   ie_ratio = max(1.5, ie_ratio - 0.1);
 
 }
 
 void compute_delays() {
-  // Serial.println("compute_delays");
-
   // TODO: write tests.
   float one_cycle = 1.0 + ie_ratio;
   inhale_hold_time = int((float)cycle_time / one_cycle);
@@ -221,8 +187,6 @@ void compute_delays() {
 
   settings_dump();
 }
-
-
 
 void state_dump(char *comment) {
   // sprintf(msg, "%ld (%ld) %.10s %d ", now, next_appt, comment, current_state);
