79a47d8f3550b5baf535589afde8fe337caeee00
David Hempy <dhempy@users.noreply.github.com>
2020-04-13 15:52:28 -0400

    Add a test that references one of our functions.

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source 'https://rubygems.org'
 gem 'arduino_ci'
+
---
+++ air_assist/test/test_main.cpp
@@ -0,0 +1,9 @@
+#include <ArduinoUnitTests.h>
+#include "../air_assist.ino"
+
+unittest(your_test_name)
+{
+  assertEqual("I don't know what", cycleRESPIRATION());
+}
+
+unittest_main()
