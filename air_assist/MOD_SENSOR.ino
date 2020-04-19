5d4532c88daa833c47f2ab3865a1365d7d1c774d
David Hempy <dhempy@users.noreply.github.com>
2020-04-19 12:37:53 -0400

    Average working on LCD. Deal only in hPa now...not Pa.

--- air_assist/MOD_SENSOR.ino
+++ air_assist/MOD_SENSOR.ino
@@ -4,10 +4,10 @@
 #define SNR_MPR_RST_PIN -1
 #define SNR_MPR_EOC_PIN -1
 
-#define HISTORY_SIZE 20
+#define HISTORY_SIZE 10
 
-unsigned long pressure_pa;  // Most recent pressure reading, in pascals. (typ. 97,999 )
-unsigned long pressure_history[HISTORY_SIZE];
+int pressure_hPa;  // Most recent pressure reading, in hectopascals. (typ. 970, range 500 - 1200 )
+int pressure_history[HISTORY_SIZE];
 
 Adafruit_MPRLS MPR = Adafruit_MPRLS(SNR_MPR_RST_PIN, SNR_MPR_EOC_PIN);
 
@@ -21,47 +21,64 @@ void init_mod_sensor() {
   }
 
   // seed pressure_history with current val:
-  pressure_pa = read_pressure();
-  for(int n = 0; n < HISTORY_SIZE; n++) { pressure_history[n] = pressure_pa; }
+  read_pressure();
+  for(int n = 0; n < HISTORY_SIZE; n++) { pressure_history[n] = pressure_hPa; }
 }
 
-unsigned long read_pressure() {
-  float pressure_hPa;
-  pressure_hPa = MPR.readPressure(); // pressure in hectopascals (float)
-  return pressure_hPa * 100;         // pressure in pascals (int)
+int read_pressure() {
+  return pressure_hPa = MPR.readPressure(); // pressure in hectopascals (int)
 }
 
 //Loads sensor data
 void snr_check() {
-  unsigned long start;
-  float pressure_hPa;
+  int start;
+  // int pressure_hPa;
 
   static byte newest = -1;
 
   start = millis();
-  pressure_hPa = MPR.readPressure();
-  pressure_pa = pressure_hPa * 100.0; // pressure_pa is int
+  read_pressure();
+  // pressure_hPa = MPR.readPressure();
+  // pressure_pa = pressure_hPa * 100.0; // pressure_pa is int
 
   newest = ++newest % HISTORY_SIZE;
-  pressure_history[newest] = pressure_pa;
-  unsigned long avg = avg_pressure();
+  pressure_history[newest] = pressure_hPa;
+  int avg = avg_pressure();
+  int diff = avg - pressure_hPa;
 
-  snprintf(msg, MSG_LEN, "%8ld Pressure (pa): %lu  avg: %lu  diff: %d   ",
-                          start, pressure_pa, avg, avg - pressure_pa);
+  snprintf(msg, MSG_LEN, "%d Pressure (pa): %d avg: %d diff: %d",
+                          start, pressure_hPa, avg, diff);
   dprintln(msg);
 
-  lcd_print(String(pressure_pa), 0, 0);
+
+  snprintf(msg, MSG_LEN, " %4d Pa", pressure_hPa);
+  lcd_print(msg, 7, 0);
+  snprintf(msg, MSG_LEN, " %4d Pa av", avg);
+  lcd_print(msg, 7, 1);
+  snprintf(msg, MSG_LEN, " %4d Pa Δ", diff);
+  lcd_print(msg, 7, 2);
+
+  // snprintf(msg, MSG_LEN, "%d %d %d %d", pressure_history[0], pressure_history[1], pressure_history[2], pressure_history[3] );
+  // lcd_print(msg, 0, 3);
+
+  // int l = pressure_hPa;
+  // snprintf(msg, MSG_LEN, " %4ld hPa", l);
+  // lcd_print(msg, 7, 3);
 
   int elapsed = millis() - start;
   if (elapsed > 100) { dprint("SLOW:"); dprintln(elapsed); }
 }
 
-long unsigned int avg_pressure() {
-  long unsigned int sum = 0;
+int avg_pressure() {
+  int sum = 0;
 
   for(int n = 0; n < HISTORY_SIZE; n++) {
     sum += pressure_history[n];
   }
+  dprint(" sum:"); dprint(sum);
+  dprint(" len:"); dprint(HISTORY_SIZE);
+  dprint(" avg:"); dprint(sum / HISTORY_SIZE);
+  dprintln();
 
   return sum / HISTORY_SIZE;
 }
