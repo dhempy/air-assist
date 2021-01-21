5b1295e17fdbd8b0d7464ccd68b874a590f348f5
Ian Katz <ikatz@drizly.com>
2021-01-21 15:41:38 -0500

    Placeholder tests are broken; remove & fix as needed

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -1,9 +1,13 @@
 #include <ArduinoUnitTests.h>
-#include "../air_assist/air_assist.h"
+#include "air_assist.h"
 
 unittest(calcBPM)
 {
-  assertEqual("I don't know what", calcBPM());
+  // this won't work -- calcBPM() returns void
+  // assertEqual("I don't know what", calcBPM());
+
+  // example of
+  assertEqual(2.0, sqrt(4));
 }
 
 unittest_main()
